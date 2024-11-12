4169017897775162b9e0fcd2cabbd81c39c0948e
Sharif-Miah <farhansharif1410@gmail.com>
2024-11-12 18:28:14 +0600

    sorting with price

--- src/component/product/ProductList/ProductList.jsx
+++ src/component/product/ProductList/ProductList.jsx
@@ -14,9 +14,11 @@ const ProductList = () => {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {filtered.map((product) => (
-              <SingleProduct key={product.id} product={product} />
-            ))}
+            {filtered
+              .sort((a, b) => a.price - b.price)
+              .map((product) => (
+                <SingleProduct key={product.id} product={product} />
+              ))}
           </div>
         </div>
       </div>
